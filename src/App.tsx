f40e1e47a0a9f6b9866fe1c8762aee1cf86a3dae
ivanxfjiang(姜希凡) <ivanxfjiang@tencent.com>
2018-08-03 12:07:53 +0800

    add just a button in antd

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 import logo from './logo.svg';
 
+import { Button } from 'antd';
+
 class App extends React.Component {
   public render() {
     return (
@@ -14,6 +16,8 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <Button>Just a Button</Button>
       </div>
     );
   }
